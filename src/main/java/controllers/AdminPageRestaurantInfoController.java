8e1829a78e1be71cee39f1e1ef62ae396412f78b
spitsynv2 <vadim.spitsyn02@gmail.com>
2023-11-23 14:22:32 +0100

    added buttons to restaurant-page-view.fxml

--- src/main/java/controllers/AdminPageRestaurantInfoController.java
+++ src/main/java/controllers/AdminPageRestaurantInfoController.java
@@ -1,5 +1,38 @@
 package controllers;
 
-public class AdminPageRestaurantInfoController {
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class AdminPageRestaurantInfoController extends AdminPageControllerAbstract {
+
+    @FXML private Label sub_header;
+    @FXML private Label sub_header2;
+    @FXML private Label name_label;
+    @FXML private Label email_label;
+    @FXML private Label phone_label;
+    @FXML private Label address_label;
+
+    @FXML private TextField name_field;
+    @FXML private TextField email_field;
+    @FXML private TextField phone_field;
+    @FXML private TextArea address_field;
+
+
+    @Override
+    protected void handleChangeColorMode() {
+        if (islightMode){
+
+        }else {
+
+        }
+        super.handleChangeColorMode();
+    }
+
+    @FXML
+    private void handleChangeDataButton() {
+
+    }
 
 }
